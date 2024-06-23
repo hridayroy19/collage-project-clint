13f4ba4de2a2fa47250392396157aa0399b2424f
hridayroy19 <hrhridoyroy503@gmail.com>
2024-06-24 00:49:36 +0600

    crate a about section

--- src/components/mainLayout/LayOut.jsx
+++ src/components/mainLayout/LayOut.jsx
@@ -1,9 +1,12 @@
 import Home from "../../pages/Home";
+import About from "../homeComponents/about/About";
+
 
 const LayOut = () => {
   return (
     <div className=" w-full text-[#112A46] bg-[#e4e4f0] 2xl:w-[1440px] 2xl:mx-auto overflow-x-hidden">
       <Home></Home>
+      <About/>
     </div>
   );
 };
