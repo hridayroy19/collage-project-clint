b2918ecdd88b188bf11884965355b04375b6b3a2
test <shreeuttomr@gmail.com>
2024-06-24 00:11:51 +0600

    main layout updated

--- src/components/mainLayout/LayOut.jsx
+++ src/components/mainLayout/LayOut.jsx
@@ -1,8 +1,8 @@
 
 const LayOut = () => {
   return (
-    <div>
-      <p> hello </p>
+    <div className=" w-full text-[#112A46] bg-[#e4e4f0] 2xl:w-[1440px] 2xl:mx-auto overflow-x-hidden">
+      <p className="text-5xl"> hello </p>
     </div>
   );
 };
