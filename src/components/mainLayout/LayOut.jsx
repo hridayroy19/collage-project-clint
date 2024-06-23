383078307397b3c0729c401c8d16a9c2a6bda984
test <shreeuttomr@gmail.com>
2024-06-24 00:16:27 +0600

    home page setup

--- src/components/mainLayout/LayOut.jsx
+++ src/components/mainLayout/LayOut.jsx
@@ -1,8 +1,9 @@
+import Home from "../../pages/Home";
 
 const LayOut = () => {
   return (
     <div className=" w-full text-[#112A46] bg-[#e4e4f0] 2xl:w-[1440px] 2xl:mx-auto overflow-x-hidden">
-      <p className="text-5xl"> hello </p>
+      <Home></Home>
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className=" text-3xl text-center">
+            <p>Home page</p>
+        </div>
+    );
+};
+
+export default Home;
