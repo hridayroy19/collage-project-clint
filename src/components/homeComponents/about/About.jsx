7da0fdbde40dae45a53e3294d959b38f6711abe5
hridayroy19 <hrhridoyroy503@gmail.com>
2024-06-24 12:36:28 +0600

    crror solved

--- src/components/homeComponents/about/About.jsx
+++ src/components/homeComponents/about/About.jsx
@@ -10,17 +10,17 @@ export class About extends Component {
           <div className="flex flex-col lg:flex-row justify-center gap-6 lg:gap-12">
             {/* image start */}
             <div className="py-14 flex justify-center lg:justify-start">
-              <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-                <div class=" h-full ">
+              <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+                <div className=" h-full ">
                   <img
-                    class="h-full w-full max-w-md  object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    className="h-full w-full max-w-md  object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
                     src="https://i.ibb.co/8D9Wfz1/bablu-patel-Byhm-wlbw-SQ-unsplash.jpg"
                     alt=""
                   />
                 </div>
-                <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-                <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-                  <p class="mb-3 text-3xl font-bold font-mono text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+                <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                  <p className="mb-3 text-3xl font-bold font-mono text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                     Aptouch Polytechnic Institute Dinajpur
                   </p>
                 </div>
@@ -54,21 +54,6 @@ export class About extends Component {
             </div>
           </div>
         </div>
-        {/* tresing */}
-
-        {/* <div class="flex min-h-screen items-center justify-center">
-    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-      <div class="h-96 w-72">
-        <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="https://i.ibb.co/8D9Wfz1/bablu-patel-Byhm-wlbw-SQ-unsplash.jpg" alt="" />
-      </div>
-      <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-      <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Aptouch Polytechnic Institute
-        Dinajpur</p>
-        <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button>
-      </div>
-    </div>
-        </div> */}
       </div>
     );
   }
