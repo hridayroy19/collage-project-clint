bf9046727f43edcedfaa2f3eae71cc4e5eac4915
hridayroy19 <hrhridoyroy503@gmail.com>
2024-08-04 11:19:29 +0600

    carate a conteact page

--- src/utils/router/Router.jsx
+++ src/utils/router/Router.jsx
@@ -1,12 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import LayOut from "../../components/mainLayout/LayOut";
+import Home from "../../pages/Home";
+import Contact from "../../pages/contact/Contact";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element:<LayOut/>
-     ,
+      element:<LayOut/>,
+      children:[
+        {
+          path:'/',
+          element:<Home/>
+        },
+        {
+          path:'contact',
+          element:<Contact/>
+        }
+      ]
     },
   ]);
 
