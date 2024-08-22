be1f76d24cd3db0a6c692c73539b80217505b771
hridayroy19 <hrhridoyroy503@gmail.com>
2024-08-22 22:04:46 +0600

    image zoom in out desing

--- src/pages/photoGellery/Gallery.jsx
+++ src/pages/photoGellery/Gallery.jsx
@@ -1,42 +1,48 @@
-import { data } from 'autoprefixer';
-import React, { useContext, useEffect, useState } from 'react';
+import { data } from "autoprefixer";
+import React, { useContext, useEffect, useState } from "react";
 
 const Gallery = () => {
+  const [image, setImage] = useState([]);
 
-const [image, setImage] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/public/imag.json");
+        const data = await response.json();
+        setImage(data);
+      } catch (error) {
+        console.log(error, "fetch data error");
+      }
+    };
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/public/imag.json");
-      const data = await response.json();
-      setImage(data);
-    } catch (error) {
-      console.log(error, "fetch data error");
-    }
-  };
+    fetchData(); // Call the fetchData function
+  }, []);
 
-  fetchData(); // Call the fetchData function
-}, []);
-     
+  return (
+    <div>
+      {/* banner section*/}
+      <div className=" mb-10 w-full">
+        <img
+          src="https://i.ibb.co/0FWcGCS/fe458127812043-5636b276d6b43.jpg"
+          className="w-full h-[45vh] object-cover"
+          alt=""
+        />
+      </div>
 
-    return (
-        <div>
-            {/* banner section*/}
-            <div  className=' mb-10 w-full'>
-                <img src="https://i.ibb.co/0FWcGCS/fe458127812043-5636b276d6b43.jpg" className='w-full h-[45vh] object-cover' alt="" />
-            </div>
-
-
-            {/* image section  */}
-            <div className=' grid mb-24 grid-cols-4 gap-3 w-full px-3   md:px-5 xl:px-16  '>
-                { image?.map((img , id)=> <div key={id}>
-                    <img src={img.url} alt="imge" className=' rounded-lg w-full h-52 object-cover' />
-                </div> ) }
-            </div>
+      {/* image section  */}
+      <div className=" grid mb-24 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-2 w-full px-3   md:px-5 xl:px-16   ">
+        {image?.map((img, id) => (
+          <div className="overflow-hidden " key={id}>
+            <img
+              src={img.url}
+              alt="imge"
+              className=" rounded-lg w-full h-52 object-cover transform duration-300 ease-in-out hover:scale-110"
+            />
+          </div>
+        ))}
+      </div>
     </div>
-        
-    );
+  );
 };
 
 export default Gallery;
