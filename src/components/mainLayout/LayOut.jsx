b5e82059ed7436c17fd1ded1c474f66a282b1ce6
TomorChandraRoy <tomorchandraroy884@gmail.com>
2024-08-24 01:43:59 +0600

    navbar create

--- src/components/mainLayout/LayOut.jsx
+++ src/components/mainLayout/LayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../sharedcomponents/Footer";
+import Navbar from "../../sharedcomponents/Navbar";
 
 const LayOut = () => {
   return (
     <div className=" w-full text-[#112A46] bg-[#FFF7F7] 2xl:w-[1440px] 2xl:mx-auto overflow-x-hidden">
+        <Navbar/>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
