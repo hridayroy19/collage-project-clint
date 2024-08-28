3bf2902913b2bfb9ef17a3b80b7e7fcbc5610b1a
hridayroy19 <hrhridoyroy503@gmail.com>
2024-08-28 11:54:36 +0600

    crate a pagination

--- src/pages/academics/Notice.jsx
+++ src/pages/academics/Notice.jsx
@@ -45,41 +45,129 @@ const Notice = () => {
       </div>
       {/* all notice */}
       <div className="mb-11 mt-9     ">
-         <h3 className="  text-xl font-serif"> All Notices</h3>
+        <h3 className="  text-xl font-serif"> All Notices</h3>
 
-         {/* tabil sectio */}
+        {/* tabil sectio */}
 
-<div className="overflow-x-auto mt-7">
-  <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-    <tbody className="divide-y divide-gray-200">
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">John Doe</td>
-        
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">View</td>
-        <td className="whitespace-nowrap px-4 py-2">
-        Today
-        </td>
-        <td className="whitespace-nowrap px-4 py-2">
-        2.20pm 
-        </td>
-      </tr>
-      <tr>
-        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Gary Barlow</td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
-      
-        <td className="whitespace-nowrap px-4 py-2">
-        Today   
-        </td>
-        <td className="whitespace-nowrap px-4 py-2">
-        2.20pm 
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700  "> <button className="border">View</button></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+        <div className="overflow-x-auto mt-7">
+          <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+            <tbody className="divide-y divide-gray-200">
+              <tr>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  John Doe
+                </td>
 
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  Web Developer
+                </td>
+                <td className="whitespace-nowrap px-4 py-2">Today</td>
+                <td className="whitespace-nowrap px-4 py-2">2.20pm</td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700  ">
+                  {" "}
+                  <button className="border px-2 py-1 border-green-500 text-green-700 font-serif hover:bg-green-700 hover:text-white ">
+                    View
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Gary Barlow
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  Singer
+                </td>
+
+                <td className="whitespace-nowrap px-4 py-2">Today</td>
+                <td className="whitespace-nowrap px-4 py-2">2.20pm</td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700  ">
+                  {" "}
+                  <button className="border px-2 py-1 border-green-500 text-green-700 font-serif hover:bg-green-700 hover:text-white ">
+                    View
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {/*  pagination */}
+
+        <div className=" mt-10">
+          <ol className="flex justify-center gap-1 text-xs font-medium">
+            <li>
+              <a
+                href="#"
+                className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+              >
+                <span className="sr-only">Prev Page</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-3"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+              >
+                1
+              </a>
+            </li>
+
+            <li className="block size-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white">
+              2
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+              >
+                3
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+              >
+                4
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+              >
+                <span className="sr-only">Next Page</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-3"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );
