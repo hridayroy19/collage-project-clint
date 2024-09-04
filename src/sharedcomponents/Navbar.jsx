68489cc16a9948ee9a7a263dc1630c6d095e4f52
hridayroy19 <hrhridoyroy503@gmail.com>
2024-09-04 15:49:22 +0600

    crate a instrctor page and responsive

--- src/sharedcomponents/Navbar.jsx
+++ src/sharedcomponents/Navbar.jsx
@@ -232,7 +232,7 @@ const Navbar = () => {
                 </li>
                 <li id="aetive" className="uppercase border-b py-3">
                   <NavLink
-                    to="*"
+                    to="instructor"
                     className="hover:text-[#007bff] hover:fill-[#007bff] text-gray-700 font-serif font-semibold text-[15px] block"
                   >
                     {" "}
